066a16af48966bd91c5950c865dff94630a6ae0c
Dave Long <dave@longwaveconsulting.com>
2010-12-21 11:58:51 +0000

    Toggle price prefix/suffix in admin UI with JavaScript.

--- uc_coupon.admin.js
+++ uc_coupon.admin.js
@@ -1,6 +1,15 @@
 // $Id$
 
 Drupal.behaviors.ucCouponAdmin = function(context) {
+  $('#edit-discount', context).keyup(function() {
+    if (this.value.indexOf('%') == -1) {
+      $(this).siblings('span').show();
+    }
+    else {
+      $(this).siblings('span').hide();
+    }
+  });
+
   $('input[name=apply_to]', context).click(function() {
     if (this.value == 'cheapest' || this.value == 'expensive') {
       $('#edit-apply-count-wrapper').show();
